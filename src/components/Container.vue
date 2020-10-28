bf014fc5068e4b6ac8c2c51c4ce851dfcaac0cf7
Neill Somerville <neill.somerville@gmail.com>
2020-10-28 02:49:08 -0700

    vuetify setup, login cmp, nav bar

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify App Bar</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn>Home</v-btn>
+      <v-btn>Login</v-btn>
+    </v-app-bar>
+    <v-content>
+      <Login></Login>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Login from "./Login";
+export default {
+  name: "Container",
+  components: { Login },
+};
+</script>
+
+<style>
+</style>
