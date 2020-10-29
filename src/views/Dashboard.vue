0304caf4dc6d97c5348afd04764f8a2f53a90393
Neill Somerville <neill.somerville@gmail.com>
2020-10-29 01:44:51 -0700

    grid, breakpoints

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,55 +1,84 @@
 <template>
-  <div>
+  <v-container>
     <h1>Dashboard</h1>
-    <v-data-table
-      :headers="headers"
-      :items="employees"
-      class="elevation-1"
-      @click:row="selectRow"
-      multi-sort
-    ></v-data-table>
 
-    <v-snackbar v-model="snackbar">
-      You have selected {{ currentName }}, {{currentTitle}}
+    <!-- Graph -->
+    <v-row>
+      <v-col v-for="sale in sales" :key="`${sale.title}`" lg="4" md="4" sm="12">
+        <SalesGraph :sale="sale" />
+      </v-col>
+    </v-row>
 
-      <template v-slot:action="{ attrs }">
-        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
-          Close
-        </v-btn>
-      </template>
+    <!-- Stats -->
+    <v-row>
+      <v-col
+        v-for="statistic in statistics"
+        :key="`${statistic.title}`"
+        lg="3"
+        md="6"
+        sm="12"
+      >
+        <StatisticCard :statistic="statistic" />
+      </v-col>
+    </v-row>
+
+    <!-- Table -->
+    <v-row>
+      <v-col sm="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col sm="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <!-- Alert -->
+    <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
+      You have selected {{ selectedEmployee.name }},
+      {{ selectedEmployee.title }}
+      <v-btn color="pink" text @click="snackbar = false"> Close </v-btn>
     </v-snackbar>
-  </div>
+  </v-container>
 </template>
 
 <script>
-import employeeData from "../data/employees.json";
+import EmployeesTable from "../components/EmployeesTable";
+import EventTimeline from "../components/EventTimeline";
+import SalesGraph from "../components/SalesGraph";
+import StatisticCard from "../components/StatisticCard";
+
+import employeesData from "../data/employees.json";
+import timelineData from "../data/timeline.json";
+import salesData from "../data/sales.json";
+import statisticsData from "../data/statistics.json";
 
 export default {
+  name: "DashboardPage",
+  components: {
+    EmployeesTable,
+    EventTimeline,
+    SalesGraph,
+    StatisticCard,
+  },
   data() {
     return {
-      currentName: "",
-      currentTitle: "",
+      employees: employeesData,
+      sales: salesData,
+      selectedEmployee: {
+        name: "",
+        title: "",
+      },
       snackbar: false,
-      headers: [
-        { text: "Employee Id", value: "id" },
-        { text: "Name", value: "name" },
-        { text: "Position", value: "title" },
-        { text: "Salary", value: "salary" },
-      ],
-      employees: employeeData,
+      statistics: statisticsData,
+      timeline: timelineData,
     };
   },
   methods: {
-    selectRow(event) {
+    setEmployee(event) {
       this.snackbar = true;
-      this.currentName = event.name;
-      this.currentTitle = event.title;
+      this.selectedEmployee.name = event.name;
+      this.selectedEmployee.title = event.title;
     },
   },
 };
 </script>
-
-<style>
-</style>
-
-  
